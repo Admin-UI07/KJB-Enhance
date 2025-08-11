f52d143b49dc8c8673b78b08bf0addfe79b3bb7d
Admin-UI07 <ummercool559@gmail.com>
2025-08-11 10:11:34 +0800

    added logic to see scroll progress

--- src/index.js
+++ src/index.js
@@ -41,3 +41,31 @@ closeBtn.addEventListener('click', () => {
  closeBtn.classList.add('hidden');
  menuMobile.classList.toggle('hidden');
 });
+
+// scrollToTop
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById('progress');
+  let progressValue = document.getElementById('progress-value');
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  // console.log(calcHeight);
+  let scrollValue = Math.round((pos * 100)/calcHeight);
+  // console.log(Math.round((pos * 100) / calcHeight));
+
+  if (pos > 300) {
+    scrollProgress.classList.remove('hidden');
+    scrollProgress.classList.add('grid');
+  } else {
+    scrollProgress.classList.add('hidden');
+  }
+
+  scrollProgress.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+  });
+
+  scrollProgress.style.background = `conic-gradient(#0cdb71 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
