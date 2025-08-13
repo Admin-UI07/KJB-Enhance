02b6c8a389fec06841af8ca6d9982f6387cdf2f6
Admin-UI07 <ummercool559@gmail.com>
2025-08-13 23:22:39 +0800

    added food section logic

---
+++ add_food_product.php
@@ -0,0 +1,54 @@
+<?php 
+
+ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['sbmt_foods'])) {
+  $foods_to_replace = $_POST['foods_replace'];
+
+  if (!empty($foods_to_replace)) {
+   $foods_current_data = $pdo->prepare("SELECT * FROM food_products WHERE id = ?");
+   $foods_current_data->execute([$foods_to_replace]);
+   $foods_row = $foods_current_data->fetch(PDO::FETCH_ASSOC);
+  } else {
+   $foods_row = [];
+  }
+
+  $foods_name = !empty($_POST['foods_name']) ? $_POST['foods_name'] : $foods_row['name'];
+  $foods_price = !empty($_POST['foods_price']) ? $_POST['foods_price'] : $foods_row['price'];
+  $foods_img_name = $foods_row['image'] ?? '';
+
+  if ($_FILES['foods_image']['error'] == 0) {
+   if (!file_exists('foods_img')) {
+    mkdir('foods_img', 0777, true);
+   }
+
+   $foods_img_name = $_FILES['foods_image']['name'];
+   $foods_img_tmp = $_FILES['foods_image']['tmp_name'];
+   $foods_img_folder = 'foods_img/' . $foods_img_name;
+
+   move_uploaded_file($foods_img_tmp, $foods_img_folder);
+  }
+
+  if (!empty($foods_to_replace)) {
+   $stmt = $pdo->prepare("UPDATE food_products SET
+    image = ?,
+    name = ?,
+    price = ?
+    WHERE id = ?
+   ");
+   $stmt->execute([$foods_img_name, $foods_name, $foods_price, $foods_to_replace]);
+  } else if (empty($foods_to_replace)) {
+   $foods_count = $pdo->query("SELECT COUNT(*) FROM food_products")->fetchColumn();
+
+   if ($foods_count >= 9) {
+    unlink($foods_img_folder);
+    die("Maximum of 9 products reached.");
+   }
+
+   $stmt = $pdo->prepare("INSERT INTO food_products
+   (image, name, price) VALUES (?, ?, ?)");
+   $stmt->execute([$foods_img_name, $foods_name, $foods_price]);
+  }
+
+  header("Location: admin.php");
+  exit();
+ }
+?>
